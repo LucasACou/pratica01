9fe8c8fab236076ddf4b248792481398f61157fa
LucasACou <111434382+LucasACou@users.noreply.github.com>
2022-08-31 12:51:30 +0000

    fixing

--- .ccls-cache/@home@runner@pratica01/questao01.c
+++ .ccls-cache/@home@runner@pratica01/questao01.c
@@ -1,14 +1,20 @@
 //Faça um programa em C que calcule a média final a partir da fórmula (0,4 x A1) + (0,6 x A2). Considere A1 e A2 números reais entre 0 a 10.
+
 #include <stdio.h>
 
 int main(void) 
 {
   float a1;
   float a2;
+  printf("coloque nota a1:");
   scanf("%f", &a1);
+  printf("coloque nota a2:");
   scanf("%f", &a2);
   float media = 0.4 * a1 + 0.6 * a2;
   printf("%f", media);
+  
+  
+  
   return 0;
 
   
--- .ccls-cache/@home@runner@pratica01/questao02.c
+++ .ccls-cache/@home@runner@pratica01/questao02.c
@@ -1,8 +1,16 @@
+//Faça um programa em C que calcule a área de um triângulo (a = base x altura / 2). Considere base e altura números inteiros maior que 0
 #include<stdio.h>
 
 int main(void)
-{
-
+{ 
+  float base;
+  float altura;
+  printf("coloque base:");
+  scanf("%f", &base);
+  printf("coloque altura:");
+  scanf("%f", &altura);
+  float area = base * altura / 2;
+  printf("%f", area);
 
 
 
